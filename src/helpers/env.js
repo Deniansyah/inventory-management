fc30686c3110ed2d3e0f4a82feb5fe1c637ed37b
Deniansyah <deniansyah814@gmail.com>
2024-03-09 10:56:49 +0700

    delete env useless

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const DB_URL = process.env.DB_URL;
 const DB_SUPABASE = process.env.DB_SUPABASE;
-const DB_HOST = process.env.DB_HOST;
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
 const JWT_SECRET = process.env.JWT_SECRET;
 const CLOUD_NAME = process.env.CLOUD_NAME; 
 const CLOUDINARY_KEY = process.env.CLOUDINARY_KEY;
@@ -16,10 +12,6 @@ module.exports = {
   PORT,
   DB_URL,
   DB_SUPABASE,
-  DB_HOST,
-  DB_NAME,
-  DB_USER,
-  DB_PASS,
   JWT_SECRET,
   CLOUD_NAME,
   CLOUDINARY_KEY,
